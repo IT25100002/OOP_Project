1cdf0eae5932934453b045db0d5b32d9d25aa887
kaveesha <IT25100066@my.sliit.lk>
2026-04-29 18:04:29 +0530

    Clear the crashed files

--- README.md
+++
@@ -1,2 +0,0 @@
-# OOP_Project
-Home Tutor Search and Booking System For SE1020
--- my-react-app/src/Components/Tutor/Tutor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Tutor() {
-  return (
-    <div>Tutor</div>
-  )
-}
-
-export default Tutor
