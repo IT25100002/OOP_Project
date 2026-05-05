60ae2b8c57d37ff77d31b416f3724887ebf0294a
IT25100002 <IT25100002@my.sliit.lk>
2026-05-05 17:32:21 +0530

    Add Massage module with service, repository, and REST controller
    - Added Massage entity
    - Implemented MassageRepository for data access
    - Created MassageService for business logic
    - Added MassageRestController for API endpoints

---
+++ back_end/src/main/java/com/university/hometutor/messaging/MassageRepository.java
@@ -0,0 +1,13 @@
+package com.university.hometutor.messaging;
+
+import com.university.hometutor.usermanagement.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.university.hometutor.messaging.Massage;
+import java.util.List;
+
+@Repository
+public interface MassageRepository extends JpaRepository<Massage, Long> {
+    List<Massage> findBySender(User sender);
+}
